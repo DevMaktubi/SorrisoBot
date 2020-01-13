4308ea829846e8e303a4ac7984649155418a7761
MaktubDEV <brenoluiz1@gmail.com>
2020-01-13 10:17:11 -0300

    Inicializando testes

---
+++ server.js
@@ -0,0 +1,12 @@
+var Twit = require('twit')
+require('dotenv').config()
+
+
+
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,
+})
