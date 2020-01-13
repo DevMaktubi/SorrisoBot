6226ee111ce77a55b2e437993d394d14834089d1
MaktubDEV <brenoluiz1@gmail.com>
2020-01-13 13:59:10 -0300

    Mudei tudo inclusive a api mas funciona perfeitamente agora. Aproveita sorriso bot"

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-var Twit = require('twit')
-require('dotenv').config()
+
+var Twitter = require('twitter');
+var config = require('./config.js');
+var Tweet = new Twitter(config);
 
 
 
-var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-  timeout_ms:           60*1000,
-})
+
+    function retweet(event) {
+      var TweetId = event.id_str
+      console.log(TweetId)
+      var Tweeter = event.user.screenName
+      Tweet.post(`statuses/retweet/${TweetId}`, function(err){
+        if(err){
+          console.log('deu erro no retweet:')
+          console.log(err)
+
+        }else {
+          console.log('RETWEETADO: ',  `https://twitter.com/${Tweeter}/status/${TweetId}`)
+        }
+      })
+
+    }
+
+    var stream = Tweet.stream('statuses/filter', {track: 'Sorriso testeteste'})
+
+    stream.on('data', retweet)
+
+    stream.on('error', function(error) {
+      console.log(error)
+    })
+
+
