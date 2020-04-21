4aec2c6c06e222ff61b985f237ab2a531200ae04
DevMaktub <brenoluiz1@gmail.com>
2020-04-20 23:33:04 -0300

    Adicionando Função de Likes

--- server.js
+++ server.js
@@ -15,36 +15,53 @@ var Tweet = new Twitter({
 
     function retweet(event) {
       // console.log(event)
-      var Tweetlocation = event.user.location
-      var TweetId = event.id_str
+      const {retweeted_status, id_str, screen_name, is_quote_status} = event
+      const {location, name} = event.user;
       // console.log(TweetId)
-      var Tweeter = event.user.name
-      if(Tweetlocation !== null && event.is_quote_status !== true){
-        Tweet.post(`statuses/retweet/${TweetId}`, function(err){
+      if(location !== null && !retweeted_status && !is_quote_status){
+        Tweet.post(`statuses/retweet/${id_str}`, function(err){
           if(err){
             if(err[0].code == 327){
               return
             }else {
-              console.log('deu erro no retweet:')
-              console.log(err)
+              console.log('deu erro no retweet: ' + err[0].message)
             }
   
           }else {
-            console.log('RETWEETADO: ',  `https://twitter.com/${Tweeter}/status/${TweetId}`)
+            console.log('RETWEETADO: ',  `https://twitter.com/${name}/status/`+id_str)
           }
         })
+        Tweet.post('favorites/create', {id: id_str}, err => {
+          if (err){
+            return console.log("Erro no like: " + err)
+          }
+          return console.log("Tweet Likado. URL do Tweet: " + `https:twitter.com/${screen_name}/status/${id_str}`)
+        })
+      
       }else {
-        return 
-      }
-
+          return 
+        }
     }
+    // function like(data) {
+    //   const {retweeted_status, id_str} = data;
+    //   const {screen_name} = data.user;
+    //   if(!retweeted_status){
+    //     Tweet.post('favorites/create', {id: id_str}, err => {
+    //       if (err){
+    //         return console.log("Erro no like: " + err)
+    //       }
+    //       return console.log("Tweet Likado. URL do Tweet: " + `https:twitter.com/${screen_name}/status/${id_str}`)
+    //     })
+    //   }
+    // }
 
     var stream = Tweet.stream('statuses/filter', {track: 'Sorriso'})
+    // var streamLikes = Tweet.stream('statuses/filter', {track: 'sorriso bot'})
 
     stream.on('data', retweet)
+    // streamLikes.on('data', like)
 
-    stream.on('error', function(error) {
-      console.log(error)
-    })
+    stream.on('error', err => console.log("Erro: " + err))
+    // streamLikes.on('error', err => console.log("Erro: " + err))
 
 
